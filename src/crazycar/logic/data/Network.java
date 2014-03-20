9f9a64e912d38fa968ef8b3f8e413863fa09a9d3
Sigurd Sippel <sigurd.sippel@haw-hamburg.de>
2014-03-20 14:29:44 +0100

    clean

---
+++ src/crazycar/logic/data/Network.java
@@ -0,0 +1,10 @@
+package crazycar.logic.data;
+
+public class Network {
+
+	public static final Network create(int xsize,int ysize, int roxelsize){
+		// TODO gerating GRID
+		return new Network();
+	}
+
+}
