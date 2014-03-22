816da1757ecc79dad5544511ff23c92f0ddbd6f7
nora <nora@bla.blub>
2014-03-22 16:31:01 +0100

    Cars=Workers

--- src/crazycar/game/Game.java
+++ src/crazycar/game/Game.java
@@ -3,35 +3,35 @@ package crazycar.game;
 import org.openspaces.core.GigaSpace;
 
 import crazycar.persistent.Space;
-import crazycar.persistent.communication.Worker;
+import crazycar.persistent.communication.CarWorker;
 
 public class Game {
 
 	/*Main Idea 
 	 * Movement:
-	 * - The Movement is executed by Workers (Tasks) by LOOPS:
-	 * 		- TAKE a Car-Location-pair from the tuplespace
-	 * 		- they move them forward 
-	 * - If an movement is succeeded, the new  car-location-pair is written into the tuplespace (from the Workers)	
-	 * - to get an accurate picture of the streets, a FIFO behavior for the locations is needed (FiFoMutexe)
+	 * - The Cars are Tasks (CarWorker) and executen LOOPS for forwarding:
+	 * 		- get the next roxel
+	 * 		- free the old one
+	 * - If an movement is succeeded, the roxel is written back into the tuplespace as BLOCKED (from the Workers)	
 	 * 
 	 * Display:
 	 * - There is exactly monitor program node (this one) 
 	 * - goal: get the next locations from the FIFO (take) 
 	 * - display in game engine
 	 * 
-	 * Location-FIFO and MUTEX (used in Worker):
-	 * - there is exactly one MutexTuple which saves the following critical path:
+	 * Location-FIFO and MUTEX (used in CAR-Task):
+     * - to get an accurate picture of the streets, a FIFO behavior for the locations is needed (FiFoMutexe)
+	 * - there is exactly one MutexTuple which saves the following critical path (the car already "took" the roxel):
 	 * 		- write the direction (NORTH/EAST/SOUTH/WEST = means "free", BLOCKED is gone) on the "old" roxel 
 	 * 		- write BLOCKED on the next roxel
 	 * 		- add +1 to the FIFO ID
-	 * 		- add FIFO-car-location-tuple to the space (which is not the same tuple as the general car-location tuple)
+	 * 		- add FIFO-car-location-tuple to the space (which is not the same tuple as the general roxel-state-tuple)
 	 * */
 	
 	/* TODO:
 	 * In this class:
 	 * - Initialise Grid
-	 * - Start Workers
+	 * - Start Cars
 	 * - Start Game
 	 * - Start GUI
 	 */
@@ -43,10 +43,10 @@ public class Game {
 	}
 
 	
-	static void initiateWorkers(int amount, GigaSpace space){
+	static void initiateCars(int amount, GigaSpace space){
 		
 		for(int i=0; i<amount;i++){
-			Worker worker=new Worker();
+			CarWorker worker=new CarWorker();
 			space.execute(worker);
 			
 		}
