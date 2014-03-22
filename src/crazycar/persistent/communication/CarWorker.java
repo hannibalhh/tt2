eac3bb2a927b125db128818ddfeba6d6a91890b6
nora <nora@bla.blub>
2014-03-22 16:31:22 +0100

    Cars=Workers

---
+++ src/crazycar/persistent/communication/CarWorker.java
@@ -0,0 +1,29 @@
+package crazycar.persistent.communication;
+
+import java.util.HashSet;
+
+import org.openspaces.core.GigaSpace;
+import org.openspaces.core.executor.Task;
+import org.openspaces.core.executor.TaskGigaSpace;
+
+public class CarWorker implements Task<HashSet<Integer>> {
+
+
+    @TaskGigaSpace
+    private transient GigaSpace gigaSpace;
+    
+	@Override
+	public HashSet<Integer> execute() throws Exception {
+		
+		/*  TODO: 
+		 * - Grab free next Area  
+		 * - Grab exclusive new-writing Tuple 
+		 * - write old location <NORTH/EAST/SOUTH/WEST> (as before, every roxel hast ONE direction)
+		 * - write new location BLOCKED
+		 * - write new Location in FIFO
+		 * - release exclusive new-writing Tuple  
+		 */
+		return null;
+	}
+
+}
